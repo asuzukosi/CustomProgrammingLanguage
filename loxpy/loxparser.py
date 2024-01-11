2551e98c5c8fab446526e79837c9cd43f9d6ff9e
asuzukosi <keloasuzu@yahoo.com>
2024-01-11 05:27:03 +0000

    feat: add base token type behaviour

--- loxpy/loxparser.py
+++ loxpy/loxparser.py
@@ -73,7 +73,43 @@ class Scanner:
     
     def isAtEnd(self):
         return self.current >= len(self.source)
+    
+    def scanToken(self):
+        c = self.advance()
+        
+        match c:
+            case '(':
+                self.addToken(TokenType.LEFT_PAREN)
+            case ')':
+                self.addToken(TokenType.RIGHT_PAREN)
+            case '{': 
+                self.addToken(TokenType.LEFT_BRACE)
+            case '}': 
+                self.addToken(TokenType.RIGHT_BRACE)
+            case ',': 
+                self.addToken(TokenType.COMMA)
+            case '.':
+                self.addToken(TokenType.DOT)
+            case '-': 
+                self.addToken(TokenType.MINUS)
+            case '+': 
+                self.addToken(TokenType.PLUS)
+            case ';': 
+                self.addToken(TokenType.SEMICOLON)
+            case '*': 
+                self.addToken(TokenType.STAR)
+            case _:
+                raise Exception("Unhandled token type")
             
+            
+    def addToken(self, token_type:TokenType, literal:str=None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
+    
+    def advance(self):
+        output = self.source[self.current]
+        self.current += 1
+        return output
     
 def scanTokens(content):
     # extract the list of valid tokens from content file
